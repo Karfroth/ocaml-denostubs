761a24f1906fbe8ee312f5105610dbeb6a2d528f
Woosang Lee <recollect12@gmail.com>
2022-01-22 20:48:29 -0300

    restore example

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 rm -rf example
 mkdir example
-esy install
-esy build
-cp _esy/default/build/install/default/lib/mylib/libmylib.so example/mylib.so
-cp _esy/default/build/install/default/lib/mylib/mylib.ts example/mylib.ts
+dune build
+cp _build/install/default/lib/mylib/libmylib.so example/mylib.so
+cp _build/install/default/lib/mylib/mylib.ts example/mylib.ts
 chmod +rw example/mylib.ts
 
 # adding some init code to ts file
-echo "\nconst dylib = loadLib(\`./mylib.so\`);" >> example/mylib.ts
-echo "\nconsole.log('initializing ocaml module');" >> example/mylib.ts
+echo "" >> example/mylib.ts
+echo "const dylib = loadLib(\`./mylib.so\`);" >> example/mylib.ts
+echo "console.log('initializing ocaml module');" >> example/mylib.ts
 echo "dylib.symbols.init();" >> example/mylib.ts
 echo "console.log('1 + 3');" >> example/mylib.ts
 echo "dylib.symbols.lib_add(1, 3);" >> example/mylib.ts
