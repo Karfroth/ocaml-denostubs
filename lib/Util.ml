0fcab4324fa11ac86c57663250a8988a7d6110ec
Woosang Lee <recollect12@gmail.com>
2022-01-22 23:26:54 -0300

    add function with string ptr

--- lib/Util.ml
+++ lib/Util.ml
@@ -1,3 +1,8 @@
 let hello name = ("Hello, " ^ name) |> print_endline
 
 let add a b = a + b |> string_of_int |> print_endline
+
+let do_nativeint_string a =
+  a
+  |> Ctypes.(!@)
+  |> print_endline
